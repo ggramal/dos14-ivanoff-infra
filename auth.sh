d0e0fd068a15fc2075b48cd942eb12dc653f2854
Sergey <saeremeev@gmail.com>
2023-08-26 18:55:32 +0200

    Change script name. Modify script.

--- auth.sh
+++ auth.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # For use this script you need to install jq "sudo apt install jq"
-read -p 'Input token code: ' token_key
-read -p 'Input profile name: ' profile
+read -p 'Enter your MFA token: ' token
+read -p 'Enter profile name: ' profile
 json=$(aws sts assume-role --role-arn arn:aws:iam::546240550610:role/Admins \
 --role-session-name tf \
---serial-number arn:aws:iam::546240550610:mfa/$profile \
---token-code $token_key \
+--serial-number $( aws configure get mfa_serial --profile $profile ) \
+--token-code $token \
 --profile $profile)
 eval $(echo  $json | jq -r '.Credentials | "AccessKeyId=\(.AccessKeyId)","SecretAccessKey=\(.SecretAccessKey)","SessionToken=\(.SessionToken)"')
 export AWS_ACCESS_KEY_ID=$AccessKeyId
