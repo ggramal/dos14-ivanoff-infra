72aefbba0df81ed4da9e66de7621057d20a19442
timofey Labzeev <timosha9911@gmail.com>
2023-10-04 12:52:46 +0300

    merge branch

--- terraform/environments/prd/aws/main.tf
+++ terraform/environments/prd/aws/main.tf
@@ -5,26 +5,20 @@ terraform {
       version = "5.11.0"
     }
   }
-
   backend "s3" {
     bucket         = "dos14-tf-state"
     key            = "ivanoff/prd/aws/state.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "tf_state_ivanoff"
   }
+
 }
 
 provider "aws" {
   region = "eu-north-1"
 }
 
-locals {
-  instances = {
-    "instance1" = {
-      name = "bank-1"
-      }
-  }
-}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -48,29 +42,24 @@ module "vpcs" {
   nat_gws      = local.vpcs["ivanoff-tf"].nat_gws
   subnets      = local.vpcs["ivanoff-tf"].subnets
 }
-
-module "dns" {
-  source      = "../../../modules/aws/rout53"
-  domain_name = var.domain_name
-  records     = var.records
-  record_type = var.record_type
-
-}
-resource "aws_instance" "bank" {
-  for_each               = local.instances
-  ami                    = data.aws_ami.ubuntu.image_id
-  instance_type          = "t3.micro"
-  key_name               = "esa"
-  vpc_security_group_ids = ["sg-054db3afbc0cbfe19"]
-  tags = {
-    Name = each.value.name
-  }
-
-  provisioner "local-exec" {
-    working_dir = "/home/tima/git-flow/dos14-Labzeev_Timofey-git-flow/"
-    command     = "sleep 100 && echo $(self.public_ip) > inventory.ini && ansible-playbook -i inventory.ini playbook.yaml"
-    environment = {
-      ANSIBLE_HOST_KEY_CHECKING = "False"
-    }
-  }
+###  !!!!!!   DON`T REMOVE "bank"  !!!!!!
+#resource "aws_instance" "bank" {
+#  ami                    = data.aws_ami.ubuntu.image_id
+#  instance_type          = "t3.micro"
+#  key_name               = "gae"
+#  vpc_security_group_ids = ["sg-054db3afbc0cbfe19"]
+#}
+module "rds" {
+  source              = "../../../modules/aws/rds"
+  name                = local.rds["rds-ivanoff-tf"].name
+  engine              = local.rds["rds-ivanoff-tf"].engine
+  identifier          = local.rds["rds-ivanoff-tf"].identifier
+  allocated_storage   = local.rds["rds-ivanoff-tf"].allocated_storage
+  engine_version      = local.rds["rds-ivanoff-tf"].engine_version
+  instance_class      = local.rds["rds-ivanoff-tf"].instance_class
+  username            = local.rds["rds-ivanoff-tf"].username
+  skip_final_snapshot = local.rds["rds-ivanoff-tf"].skip_final_snapshot
+  rds-subnet          = local.rds["rds-ivanoff-tf"].rds-subnet
+  rds-sg              = local.rds["rds-ivanoff-tf"].rds-sg
+  vpc_id              = module.vpcs.vpc_id
 }
