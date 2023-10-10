b995819767577c4ebdf497d902fcd43fad5fb97b
rusLAN <ruslan.polikarpov.2014@gmail.com>
2023-10-10 20:39:46 +0000

    fix ami

--- terraform/modules/aws/asg/main.tf
+++ terraform/modules/aws/asg/main.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
     most_recent = true
     filter {
       name = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server*"]
+      values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516*"]
     }
     filter {
       name = "virtualization-type"
