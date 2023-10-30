898005fd55dc7fbe955326e1d0aba66ec8b2d9d8
timofey Labzeev <timosha9911@gmail.com>
2023-10-30 13:58:41 +0300

    mv output to output.tf

--- terraform/modules/aws/rds/main.tf
+++ terraform/modules/aws/rds/main.tf
@@ -43,16 +43,6 @@ resource "random_password" "password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-output "rds_username" {
-  description = "Username for RDS"
-  value       = aws_db_instance.postgres.username
-}
-
-output "rds_password" {
-  description = "Password for RDS"
-  value       = random_password.password.result
-  sensitive   = true
-}
 
 # create a RDS Database Instance
 resource "aws_db_instance" "postgres" {
